9c45f7dde6dd78fca50c5636bc1b5487cee54db9
Elica Yordanova <elitza_85@yahoo.co.uk>
2020-10-07 13:09:34 +0300

    Add Range attr to DogsCompetitions

--- MyPr/Models/Models/DogsCompetitions.cs
+++ MyPr/Models/Models/DogsCompetitions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Models
@@ -15,24 +16,32 @@ namespace Models
         public virtual Competition Competition { get; set; }
 
         //overall appropriate proportions
+        [Range(1,5)]
         public int BalanceRate { get; set; }
 
+        [Range(1, 5)]
         public int WeightRate { get; set; }
 
         //color, shape- typical for the breed
+        [Range(1, 5)]
         public int EyesRate { get; set; }
 
         //shape, length, position- typical for the breed
+        [Range(1, 5)]
         public int EarsRate { get; set; }
 
+        [Range(1, 5)]
         public int HeadShapeRate { get; set; }
 
         //shape, length- typical for the breed
+        [Range(1, 5)]
         public int MuzzleRate { get; set; }
 
         //accepted breed colors
+        [Range(1, 5)]
         public int ColorRate { get; set; }
 
+        [Range(5, 35)]
         public int TotalPoints { get; set; }
     }
 }
