272a137f87158866a7b96380ba70007887f649c5
Elica Yordanova <elitza_85@yahoo.co.uk>
2020-11-14 11:11:03 +0200

    Details view

---
+++ DogHub/Web/DogHub.Web.ViewModels/Competitions/CompetitionDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DogHub.Web.ViewModels.Competitions
+{
+    using System;
+
+    public class CompetitionDetailsViewModel
+    {
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Status { get; set; }
+
+        public int ParticipantsCount { get; set; }
+    }
+}
