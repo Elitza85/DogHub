564df76b2c61323347a57afa75de2b53a53f76b6
Elica Yordanova <elitza_85@yahoo.co.uk>
2020-11-17 16:46:33 +0200

    Implement js function to count points in the vote form

---
+++ DogHub/Web/DogHub.Web/wwwroot/js/CommonForms/VotingFormScripts.js
@@ -0,0 +1,31 @@
+﻿function DisplayVideo() {
+    var videoBlock = document.querySelector("#videoBlock");
+    var closeBtn = document.querySelector("#closeBtn");
+
+    if (videoBlock.style.display == 'none') {
+        videoBlock.style.display = "block";
+        closeBtn.style.display = "block";
+    }
+
+    closeBtn.addEventListener("click", () => {
+        videoBlock.style.display = "none";
+        closeBtn.style.display = "none";
+    })
+}
+
+function calc() {
+    var component = document.querySelector("#votingForm")
+    var elements = component.querySelectorAll(".rating-star");
+    var result = document.querySelector("#selected-rating");
+    var total = 0;
+    for (var i = 0; i < elements.length; i++) {
+        var currentElements = elements[i];
+        var inputs = currentElements.querySelectorAll("input[type=radio]")
+        for (var y = 0; y < inputs.length; y++) {
+            if (inputs[y].checked) {
+                total += parseInt(inputs[y].value)
+                result.textContent = total;
+            }
+        }
+    }
+};
