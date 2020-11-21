48a42ba26968d06bcaff83612fd32007f537da36
Elica Yordanova <elitza_85@yahoo.co.uk>
2020-11-21 14:43:22 +0200

    Scaffold the register form adding new field Age

---
+++ DogHub/Web/DogHub.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DogHub.Data;
+using DogHub.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DogHub.Web.Areas.Identity.IdentityHostingStartup))]
+namespace DogHub.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
