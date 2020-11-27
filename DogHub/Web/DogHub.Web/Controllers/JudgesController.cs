b24139497f3870f567b547601d8f1c6b47af32e7
Elica Yordanova <elitza_85@yahoo.co.uk>
2020-11-27 21:50:36 +0200

    Work on judge list

---
+++ DogHub/Web/DogHub.Web/Controllers/JudgesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogHub.Web.Controllers
+{
+    public class JudgesController : Controller
+    {
+        public IActionResult JudgesList()
+        {
+            return this.View();
+        }
+    }
+}
