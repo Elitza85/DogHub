e862dc7f7576a599d9f4f9bd8eabb1987e1df316
Elica Yordanova <elitza_85@yahoo.co.uk>
2020-12-09 12:43:02 +0200

    Move js for OwnerDashboard-Index to a separate js file

---
+++ DogHub/Web/DogHub.Web/wwwroot/js/UserDashboards/IndexScripts.js
@@ -0,0 +1,9 @@
+﻿var btnsSection = document.querySelector("#optionsBtns");
+var acceptBtn = document.querySelector("#acceptBtn");
+var rejectBtn = document.querySelector("#rejectBtn");
+var approvedArea = document.querySelector("#approvedArea");
+
+acceptBtn.addEventListener("click", () => {
+    btnsSection.style.display == "none";
+    approvedArea.style.display = "block";
+})
