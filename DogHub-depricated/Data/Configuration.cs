78c24e91ceef081eeefc700a159c7ee260507c37
Elica Yordanova <elitza_85@yahoo.co.uk>
2021-01-11 09:38:42 +0200

    Remove depricated folder

--- DogHub-depricated/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DogHub.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=.\SQLEXPRESS;Database=DogHub;Integrated Security=true;";
-    }
-}
